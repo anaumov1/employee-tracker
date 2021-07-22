adbd5be01d55b472f8280512484b02ed1c403830
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-22 10:56:57 -0500

    add new role

--- index.js
+++ index.js
@@ -510,3 +510,83 @@ const deleteDepartment = () => {
         });
     });
 };
+
+
+// Adds new role
+const addRole = () => {
+    const sql = `SELECT * FROM department;`
+    const departmentArr = [];
+
+    // Gets all current departments and pushes to deptArr
+    db.query(sql, (err, roleData) => {
+        if (err) throw error;
+        roleData.forEach((department) => departmentArr.push(department.name));
+    })
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addRoleName',
+            message: "Please enter the name of the new role.",
+            validate: roleNameInput => {
+                if (roleNameInput) {
+                    return true;
+                } else {
+                    console.log("  Please enter the new role's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'addRoleSalary',
+            message: "Please enter the salary of the new role.",
+            validate: roleSalaryInput => {
+                if (roleSalaryInput) {
+                    if(isNaN(roleSalaryInput)) {
+                        console.log("  Please enter a number for the salary.")
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    console.log("  Please enter the new role's salary.");
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'list',
+            name: 'addRoleDepartment',
+            message: "Please select the department the role belongs to.",
+            choices: departmentArr  
+        }
+    ])
+    .then(response => {
+        const responseDept = response.addRoleDepartment;
+        let deptID;
+        const sqlID = `SELECT * FROM department;`
+
+        // Query to find department ID
+        db.query(sqlID, (err, result) => {
+            if (err) throw err;
+
+            // Loops through departments to find the matching ID
+            result.forEach((department) => {
+                if (responseDept === department.name) {
+                    deptID = department.id;
+                }
+            })
+
+            const sql = `INSERT INTO role (title, salary, department_id) 
+                        VALUES (?,?,?)`;
+            const params = [response.addRoleName, response.addRoleSalary, deptID];
+                
+            // Query to add data to table
+            db.query(sql, params, (err, results) => {
+                if (err) throw err;
+                console.log('New role added successfully!');
+                initialPrompt();
+            })
+        })
+    });
+};
