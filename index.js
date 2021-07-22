9f087961901fc37dd4e23d44bcde41abd00b16ca
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-22 10:12:06 -0500

    get all employees function

--- index.js
+++ index.js
@@ -71,3 +71,29 @@ const initialPrompt = () => {
         }
     });
 };
+
+
+// Gets all employee data from database
+const viewAllEmployees = () => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name,
+                role.title AS title,
+                department.name AS department, role.salary,
+                CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN (employee manager) ON manager.id = employee.manager_id
+                LEFT JOIN role on employee.role_id = role.id
+                LEFT JOIN department ON department.id = role.department_id;`
+
+    
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(``);
+        console.log(``);
+        console.log(`                                   ` + 'All Employees');
+        console.log(`=============================================================================================`)
+        console.table(result);
+        console.log(`=============================================================================================`)
+        initialPrompt();
+    });
+};
